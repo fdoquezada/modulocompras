6be48d936e5cda9ef99a6434a4e1cd26c8e741d8
fernandres <fdoquezadapuno@gmail.com>
2022-08-30 00:36:11 -0400

    se encripto key

--- modulocompras/modulocomprasventas/modulocomprasventas/settings.py
+++ modulocompras/modulocomprasventas/modulocomprasventas/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+SECRET_KEY = os.environ['SECRET_KEY']
+import dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,8 +22,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
+dotenv_file = os.path.join(BASE_DIR, ".env")
+if os.path.isfile(dotenv_file):
+    dotenv.load_dotenv(dotenv_file)
+
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-u_cp0e(5dncr+sycr1f#32wczyi(0un)fu%4ydckndo9+0(=+%'
+SECRET_KEY = os.environ['SECRET_KEY'] 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
